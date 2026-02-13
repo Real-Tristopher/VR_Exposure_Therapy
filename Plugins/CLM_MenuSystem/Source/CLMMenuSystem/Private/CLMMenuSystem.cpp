6ca176ab91bdc723dfa87da007b3bf2da2b298a7
Cora Ioan-Alexandru <ionutcora@proton.me>
2026-02-13 03:44:21 +0200

    Accessibility Main Menu

---
+++ Plugins/CLM_MenuSystem/Source/CLMMenuSystem/Private/CLMMenuSystem.cpp
@@ -0,0 +1,17 @@
+﻿ #include "../Public/CLMMenuSystem.h"
+
+#define LOCTEXT_NAMESPACE "CLMMenuSystemModule"
+
+void CLMMenuSystemModule::StartupModule()
+{
+ 
+}
+
+void CLMMenuSystemModule::ShutdownModule()
+{
+ 
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(CLMMenuSystemModule, CLMMenuSystem)
---
+++ Plugins/CLM_MenuSystem/Source/CLMMenuSystem/Public/CLMMenuSystem.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class CLMMenuSystemModule: public IModuleInterface
+{
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
